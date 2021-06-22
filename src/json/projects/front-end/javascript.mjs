26e496b5e3cc3f289639593d4e6fc6901a37601b
albertoscandolara <alberto.scandolara92.training@fmail.com>
2021-06-22 08:31:14 +0200

    Added links to see built projects

--- src/json/projects/front-end/javascript.mjs
+++ src/json/projects/front-end/javascript.mjs
@@ -5,7 +5,7 @@ export const javascriptProjects = [
       "description": "Fetch a limited number of plane flights info from The OpenSky Network API, plot them on the map and update them every time interval.",
       "preview": "preview_real_time_flights_tracker",
       "difficulty": 2,
-      "projectUrl": "",
+      "projectUrl": "https://real-time-planes-tracker.netlify.app/",
       "githubUrl": "https://github.com/albertoscandolara/Real-time-planes-tracker"
     },
     {
@@ -14,7 +14,7 @@ export const javascriptProjects = [
       "description": "Two eyes on a page. Make them follow the cursor or make them blink. Don't idle or they'll get bored! ;)",
       "preview": "preview_eye_movement",
       "difficulty": 1,
-      "projectUrl": "",
+      "projectUrl": "https://eyes-movement.netlify.app/",
       "githubUrl": "https://github.com/albertoscandolara/Eye_movement"
     },
     {
